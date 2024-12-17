5e0bb9e7a0a78de12ca28200b6b849bf9dc29840
Eshanokpe Daniel <eshanokpe@gmail.com>
2024-12-17 09:19:07 +0100

    comment

--- app/Http/Controllers/User/PaymentController.php
+++ app/Http/Controllers/User/PaymentController.php
@@ -141,7 +141,8 @@ class PaymentController extends Controller
                 
                 if ($wallet) {
                     $userBalance = $wallet->balance; // Directly access balance
-
+                    dd($userBalance);
+                    dd($amount);
                     // Check if the user has sufficient balance
                     if ($userBalance >= $amount) {
                         $v = $userBalance - $amount;
