86a0d69fa96d6b24bf47fb94063c6297e4108414
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-06-01 22:20:05 +0100

    comment

--- resources/views/user/pages/notifications/notificationDetails/walletFundedNotification.blade.php
+++ resources/views/user/pages/notifications/notificationDetails/walletFundedNotification.blade.php
@@ -14,7 +14,7 @@
 
         <div class="notification-meta mt-3">
             <p>Here are the details of your transfer</p>
-            <p><strong>Amount Received:</strong> ₦{{ ($notification->data['amount']/100 ?? '0.00') }}</p>
+            <p><strong>Amount Received:</strong> ₦{{ number_format((float)(($notification->data['amount'] ?? 0) / 100), 2) }}</p>
             {{-- <p><strong>New Balance:</strong> ₦{{ ($notification->data['balance'] ?? '0.00') }}</p> --}}
             <p><strong>Transfer Date:</strong> {{ \Carbon\Carbon::parse($notification->created_at)->format('F j, Y \a\t g:i A') }}</p>
            
