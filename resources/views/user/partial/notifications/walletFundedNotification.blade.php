4c18643607493fd15ec6f9d2608f6c1fb15d3a74
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-06-01 21:48:16 +0100

    comment

--- resources/views/user/partial/notifications/walletFundedNotification.blade.php
+++ resources/views/user/partial/notifications/walletFundedNotification.blade.php
@@ -1,10 +1,7 @@
-
-
-
 <div class="notification__type--wallet">
     <h4>{{ $notification['data']['subject'] ?? 'Your Wallet Has Been Credited' }}</h4>
     <p>
-        ₦{{ number_format($notification['data']['amount'], 2) }} received
-        <small>New balance: ₦{{ number_format((float) $notification['data']['balance'], 2) }}</small>
+        ₦{{ number_format((float) ($notification['data']['amount'] ?? 0), 2) }} received<br>
+        <small>New balance: ₦{{ number_format((float) ($notification['data']['balance'] ?? 0), 2) }}</small>
     </p> 
 </div>
