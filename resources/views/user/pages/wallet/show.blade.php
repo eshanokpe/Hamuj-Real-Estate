7fbf4eeecf96f20aa162653dfbaa034fef258597
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-06-04 13:03:35 +0100

    comment

--- resources/views/user/pages/wallet/show.blade.php
+++ resources/views/user/pages/wallet/show.blade.php
@@ -120,7 +120,12 @@
                                                         <div class="card border-0 bg-light">
                                                             <div class="card-body">
                                                                 <h6 class="card-title text-muted">Recipient Details</h6>
-                                                                {{$txn->metadata }}
+                                                                {{$txn->metadata['receiver_account_number'] }}
+                                                                {{$txn->metadata['receiver_bank'] }}
+                                                                 <div class="mb-2">
+                                                                    <small class="text-muted">Account Number</small>
+                                                                    <p class="mb-0 fw-bold">{{ $txn->metadata['receiver_account_number']}}</p>
+                                                                </div>
                                                                 @if(isset($txn->bankName) || isset($txn->accountName) || isset($txn->bank_name) || isset($txn->account_name))
                                                                 <div class="mb-2">
                                                                     <small class="text-muted">Bank</small>
