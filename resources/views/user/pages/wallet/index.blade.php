31342e7c119efc6071c097f28bce89bdebac2784
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-06-04 11:04:02 +0100

    comment

--- resources/views/user/pages/wallet/index.blade.php
+++ resources/views/user/pages/wallet/index.blade.php
@@ -113,6 +113,7 @@
                                         <td style="padding: 10px;">{{ $loop->iteration }}</td>
                                        <td style="padding: 5px;">
                                         <span class="sales__report--body__text">
+                                            {{ $transaction->payment_method }}
                                             @if (isset($transaction->payment_method) && strtolower($transaction->payment_method) === 'dedicated_nuban')
                                                 Deposit
                                             @elseif(isset($transaction->payment_method))
