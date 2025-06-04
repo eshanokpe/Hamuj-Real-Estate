c2f4a5445e86161387eba3410595de1f05e36e6a
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-06-04 12:58:25 +0100

    comment

--- resources/views/user/pages/wallet/show.blade.php
+++ resources/views/user/pages/wallet/show.blade.php
@@ -72,7 +72,7 @@
 
                                                     </div>
                                                     <div class="col-md-6 text-md-end">
-                                                        <img src="{{ assets('assets/img/dohmaynlogo.png') }}" />
+                                                        <img src="{{ asset('assets/img/dohmaynlogo.png') }}" />
                                                     </div>
                                                 </div>
 
