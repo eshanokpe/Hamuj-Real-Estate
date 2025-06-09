4215af71df50703967e0be4627952095be08bfdc
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-06-09 15:10:50 +0100

    comment

--- resources/views/user/pages/notifications/notificationDetails/referralCommission.blade.php
+++ resources/views/user/pages/notifications/notificationDetails/referralCommission.blade.php
@@ -5,22 +5,21 @@
     {{-- {{ $notification }} --}}
    
     <div class="mt-4">
-        {{$notification['data']['message']}}
         <div class="alert alert-info">
             {{ $notification['data']['subject'] ?? '🎉 Referral Commission!' }}
         </div>
         <h3 class="mb-2">Transaction Details</h3>
 
-        {{-- <div class="notification-meta mt-3">
+        <div class="notification-meta mt-3">
             <p>Here are the details of your transfer</p>
             <p><strong>Amount Received:</strong> ₦{{ ($notification->data['amount'] ?? '0.00') }}</p>
             <p><strong>New Balance:</strong> ₦{{ ($notification->data['balance'] ?? '0.00') }}</p>
             <p><strong>Transfer Date:</strong> {{ \Carbon\Carbon::parse($notification->created_at)->format('F j, Y \a\t g:i A') }}</p>
            
-        </div> --}}
+        </div>
 
     <p class="mt-3">
-        You will earn 3% commission when they make their first property purchase.
+        {{$notification['data']['message']}}
     </p>
     <a class="btn btn-primary btn-large" href="{{route('user.referral.index') }}">
         View Your Referrals
