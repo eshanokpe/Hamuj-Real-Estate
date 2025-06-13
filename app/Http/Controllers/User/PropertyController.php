9c4b1cf3d2972393b433ac64b650fbe40303a4a4
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-06-13 11:51:01 +0100

    comment

--- app/Http/Controllers/User/PropertyController.php
+++ app/Http/Controllers/User/PropertyController.php
@@ -141,12 +141,15 @@ class PropertyController extends Controller
         $propertyId = decrypt($id); 
         $data['property'] = Property::findOrFail($propertyId);
         $data['propertyValuation'] = PropertyValuation::where('property_id', $data['property']->id)
-        ->when(request('filter'), function ($query, $yearFilter) {
-            return $query->whereYear('created_at', $yearFilter);
-        })
+        // ->when(request('filter'), function ($query) {
+        //     if ($year = request('filter')) {
+        //         return $query->whereYear('created_at', $year);
+        //     }
+        //     return $query;
+        // }) 
         ->orderBy('created_at', 'asc') 
         ->get(); 
-        dd($data['propertyValuation']);
+        // dd($data['propertyValuation']);
 
         $data['initialValueSum'] = PropertyValuationSummary::where('property_id', $propertyId)->value('initial_value_sum') ?? 0;
         $data['valueSum'] = $this->calculateValuationSums($data['propertyValuation']);
