d750acde84e88c7973b880077d9ad818d5ad0356
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-06-18 20:33:37 +0100

    comment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
    
     @include('home.partial.footer')
     {{-- <div style="position: fixed; bottom: 20px; right: 20px; z-index: 9999;"> --}}
-      @include('chattle::chat')
+      {{-- @include('chattle::chat') --}}
     {{-- </div> --}}
     
 </body>
