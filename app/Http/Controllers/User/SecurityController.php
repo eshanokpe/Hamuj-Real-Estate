ed921346022fadc866aceefe8705c884f0bf403a
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-07-15 12:22:40 +0100

    comment

--- app/Http/Controllers/User/SecurityController.php
+++ app/Http/Controllers/User/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller; 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
