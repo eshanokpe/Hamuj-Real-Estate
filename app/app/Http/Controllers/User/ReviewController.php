9911bc13889034613d153f1e5b0a9623c2b8ed86
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-09-06 23:31:53 +0100

    comment

--- app/app/Http/Controllers/User/ReviewController.php
+++ app/app/Http/Controllers/User/ReviewController.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Http\Controllers\User;
-use App\Http\Controllers\Controller
+use App\Http\Controllers\Controller;
 use App\Models\Review;
 use App\Models\Property;
 use Illuminate\Http\Request;
