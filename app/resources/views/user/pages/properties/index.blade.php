355c3d1495088a3e4a3cbafe150d369cbc199da0
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-10-03 17:05:58 +0100

    comment

--- app/resources/views/user/pages/properties/index.blade.php
+++ app/resources/views/user/pages/properties/index.blade.php
@@ -12,7 +12,7 @@
             <!-- dashboard container -->
             <div class="dashboard__container dashboard__reviews--container">
                 <div class="reviews__heading mb-30"> 
-                    <h2 class="reviews__heading--title">Buy Properties</h2>
+                    <h2 class="reviews__heading--title">Buy Properties222</h2>
                     <p class="reviews__heading--desc">We are glad to see you again!</p>
                 </div>
                 <div class="properties__wrapper">
