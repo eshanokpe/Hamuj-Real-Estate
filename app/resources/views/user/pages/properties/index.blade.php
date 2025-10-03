1ea41bcdbe88aa4c92228530237b4c75847ca6bc
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-10-03 17:09:48 +0100

    comment

--- app/resources/views/user/pages/properties/index.blade.php
+++ app/resources/views/user/pages/properties/index.blade.php
@@ -60,10 +60,10 @@
                                             @endif
                                         </td>
                                         <td>
-                                            <span class="properties__views">{{ number_format($item->size, 0) }} SQM </span>
+                                            <span class="properties__views">{{ number_format($item->size, 2) }} SQM </span>
                                         </td>
                                         <td>
-                                            <span class="properties__views">{{ number_format($item->available_size, 0) }} SQM </span>
+                                            <span class="properties__views">{{ number_format($item->available_size, 2) }} SQM </span>
                                         </td>
                                         <td>
                                             @if($item->status === 'sold out')
