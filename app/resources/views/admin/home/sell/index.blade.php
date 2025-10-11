0c40a7d7a2bc87db9a9c58fabdb963c206f2345e
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-10-11 14:19:21 -0700

    comment

--- app/resources/views/admin/home/sell/index.blade.php
+++ app/resources/views/admin/home/sell/index.blade.php
@@ -61,7 +61,7 @@
                                             <th>Total Assets</th>
                                             <th>Property Name</th>
                                             <th>Sold Size</th>
-                                            <th>Remaining Size</th>
+                                            {{-- <th>Remaining Size</th> --}}
                                             <th>Sale Price</th>
                                             <th>Wallet Impact</th>
                                             <th>Status</th>
@@ -87,9 +87,8 @@
                                                     </span>
                                                 </td>
                                                 <td>{{ $sell->property->name }}</td>
-                                                {{-- <td>{{ $sell->property->name }}</td> --}}
                                                 <td>{{ $sell->selected_size_land }} SQM</td>
-                                                <td>{{ $sell->remaining_size }} SQM</td>
+                                                {{-- <td>{{ $sell->remaining_size }} SQM</td> --}}
                                                 <td>₦{{ number_format($sell->total_price, 2) }}</td>
                                                 <td>
                                                     <span class="badge bg-danger">
