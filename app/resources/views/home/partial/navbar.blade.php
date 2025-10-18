fe15a1f353d03853f007b080125d020b8cc4007b
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-10-18 12:05:02 +0100

    comment

--- app/resources/views/home/partial/navbar.blade.php
+++ app/resources/views/home/partial/navbar.blade.php
@@ -40,48 +40,7 @@
                             register
                         </a>
                     </li>
-                    @guest
-                            @if (Route::has('login'))
-                                <li class="offcanvas__menu_li">
-                                    <a class="offcanvas__menu_item" href="{{ route('login') }}">
-                                        Login
-                                    </a>
-                                </li>
-                                <a class="login__register--link" href="{{ route('login') }}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users">
-                                        <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg> 
-                                        <span>Login</span>
-                                </a>
-                            @endif
-                            @if (Route::has('register'))
-                                <a class="add__listing--btn solid__btn" href="{{ route('register') }}"><span>Register</span> 
-                                </a>
-                            @endif
-
-                        @else
-                           
-                            <li class="main__menu--items">
-                                <a class="main__menu--link" href="#"> {{ Auth::user()->name }} </a>  
-                                
-                                <ul class="sub__menu">
-                                    <li class="sub__menu--items">
-                                        <a href="{{ route('user.dashboard') }}" class="sub__menu--link">Dashboard</a>
-                                        <a class="sub__menu--link" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                            document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-                                    </li>
-                                    
-           
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-
-                                </ul>
-                            </li>
-                           
-                        @endguest
+                
                 </ul>
             </nav>
            
