e814120ff4ad2a27a656914b3bb50b38ff9029a2
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-25 14:57:47 +0100

    Updated Changes

--- app/resources/views/admin/home/buy/search-results.blade.php
+++ app/resources/views/admin/home/buy/search-results.blade.php
@@ -223,6 +223,13 @@
                         {{ number_format($totalSelectedSize, 2) }} SQM
                     </span>
                 </td>
+                <td class="text-primary">
+                        <span class="d-flex align-items-center">
+                            <i class="las la-money-bill-wave me-2"></i>
+                            ₦{{ number_format($totalPriceSum ?? 0, 2) }}
+                        </span>
+                        <small class="text-muted d-block">Total Price Sum</small>
+                </td>
                 <td>
                     <div class="progress mb-2" style="height: 8px;">
                         <div class="progress-bar bg-success" role="progressbar" 
@@ -264,6 +271,9 @@
                             </span>
                         </div>
                     </div>
+                </td>
+                <td>
+                   
                 </td>
             </tr>
         </tfoot>
