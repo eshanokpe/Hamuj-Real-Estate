b021173f2c0e9c1748fc2d22866b9a5643a82bba
infoscert <eshanokpe@gmail.com>
2026-05-23 18:40:03 +0100

    chore: update 2 file files

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -81,7 +81,7 @@ class User extends Authenticatable
     public function sendPasswordResetDeepLinkNotification($token)
     {
         $this->notify(new ResetPasswordDeepLink($token));
-    }
+    } 
 
     public function buys()
     {
