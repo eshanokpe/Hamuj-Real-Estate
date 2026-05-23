88dc24b53096e114ca281b18a2686e1b61702b08
infoscert <eshanokpe@gmail.com>
2026-05-23 18:42:27 +0100

    chore: update 2026_05_23_173645_create_password_reset_tokens_table

--- app/database/migrations/2026_05_23_173645_create_password_reset_tokens_table.php
+++ app/database/migrations/2026_05_23_173645_create_password_reset_tokens_table.php
@@ -6,15 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
+            $table->string('email', 191)->primary();
+            $table->string('token', 191);
             $table->timestamp('created_at')->nullable();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('password_reset_tokens');
